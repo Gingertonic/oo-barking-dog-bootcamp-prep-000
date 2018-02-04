c34f6e0c9342249d56e65db9ba5d7226a48dacbe
Beth Schofield <bethmschofield@gmail.com>
2018-02-04 07:01:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def initialize(name)
+    @name = name
+  end
+
+  def name= (newname)
+    @name = newname
+  end
+
+  def name
+    "#{@name}"
+  end
+
+  def bark
+    puts "Woof!"
+  end
+end
